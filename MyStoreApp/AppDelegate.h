2df38e74d538224807086a547f2f2a134bff98ac
Roark Stockwell <roarkstockwell@gmail.com>
2015-08-31 09:20:43 -0600

    Initial Commit

---
+++ MyStoreApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyStoreApp
+//
+//  Created by Roark on 8/31/15.
+//  Copyright (c) 2015 Roark Stockwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyStoreApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyStoreApp
+//
+//  Created by Roark on 8/31/15.
+//  Copyright (c) 2015 Roark Stockwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
